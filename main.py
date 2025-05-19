d4d9ee590777569c83b4bb5d045d117b2496691b
Pedro Daim <daimhpedro@gmail.com>
2025-05-18 23:10:58 -0300

    Sorted the top 10 players by rating and kd ratio.

--- main.py
+++ main.py
@@ -1,52 +1,57 @@
-#Importing necessary libraries
+#Import necessary libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#Import players and teams data and tranform the CSV files into pandas dataframes
 df_players = pd.read_csv('player_stats.csv')
+df_teams = pd.read_csv('team_stats.csv')
 
-#Display the first few rows of the dataframe
+#Display the first few rows of the dataframe df_players
 print("First Few Rows Of the Dataframe.")
 print(df_players.head())
 print("\n" + "="*50 + "\n")
 
-#Display the total rows and columns of the dataframe
+#Display the total rows and columns of the dataframe df_players
 print("Rows and Columns Total.")
 print(df_players.shape)   
 print("\n" + "="*50 + "\n")
 
-#Display the columns of the dataframe
+#Display the columns of the dataframe df_players
 print("Columns of the Dataframe.")  
 print(df_players.columns)
 print("\n" + "="*50 + "\n")
 
-#Display descriptive statistics of the dataframe
+#Display data types, null counts, memory usage.
+print("Data Types, Null Counts, Memory Usage.")
+print(df_players.info)   
+print("\n" + "="*50 + "\n")
+
+
+#Display descriptive statistics of the dataframe df_players
 print("Descriptive Statistics of the Dataframe.")       
 print(df_players.describe())
 print("\n" + "="*50 + "\n")
 
-#Display top 10 countries with the highest KD ratio
-top_10_countries = (
-    df_players.groupby('country')['kd']
-    .mean()
-    .sort_values(ascending=False)
-    .head(10)
-)
-
-print("Top 10 countries with the highest KD ratio:")
-print(top_10_countries)
+#Check for missing values in the dataframe df_players
+print("Missing Values in the Dataframe.")   
+print(df_players.isnull().sum())
 print("\n" + "="*50 + "\n")
 
-# Convert the pandas Series to a DataFrame for seaborn
-top_10_df = top_10_countries.reset_index(name='average_kd')
+# Get the top 10 players by rating
+top10_rated_players = df_players.sort_values(by='rating', ascending=False).head(10)
 
-# Create the bar plot for the top_10_countries function using seaborn
-plt.figure(figsize=(12, 8))
-sns.barplot(x='average_kd', y='country', data=top_10_df, palette='viridis')
-plt.title('Top 10 Countries with Highest Average KD Ratio')
-plt.xlabel('Average Kill/Death Ratio')
-plt.ylabel('Country')
-plt.tight_layout()
-plt.show()
+# Display the top 10 players by rating
+print("Top 10 Players by Rating.")
+print(top10_rated_players[['name', 'country', 'rating']])
+print("\n" + "="*50 + "\n")
+
+# Get the top 10 players by KD Ratio
+top10_kd_players = df_players.sort_values(by='kd', ascending=False).head(10)
+
+# Display the top 10 players by KD Ratio
+print("Top 10 Players by KD Ratio.")    
+print(top10_kd_players[['name', 'country', 'kd']])
+print("\n" + "="*50 + "\n")
 
